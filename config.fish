55242a4389ad16466e0717d048ad25c6116f3709
Vaibhav Mehrotra <vaibhavmehrotra1194@gmail.com>
2024-05-10 17:07:09 +0530

    Create config.fish

---
+++ config.fish
@@ -0,0 +1,11 @@
+function random_quote
+  set animal (random choice {default,dragon,dragon-and-cow,elephant,moose,stegosaurus,tux,vader})
+  fortune -s | cowsay -f $animal
+end
+
+if status is-interactive
+    random_quote
+    starship init fish | source
+    cd ./work
+    alias kl "kubectl"
+end
